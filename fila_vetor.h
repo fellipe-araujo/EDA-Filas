705cb4cf8404c90bedab017424a780f4c4d03280
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-10-28 14:17:09 -0300

    Added functions on .h files.

---
+++ fila_vetor.h
@@ -0,0 +1,20 @@
+#ifndef FILA_VETOR_H
+#define FILA_VETOR_H
+
+int cria_fila();
+
+int enfileira(int x);
+
+int desenfileira(int *x);
+
+int fila_cheia();
+
+int fila_vazia();
+
+int tamanho_fila();
+
+void imprime_fila();
+
+int destroi_fila();
+
+#endif
